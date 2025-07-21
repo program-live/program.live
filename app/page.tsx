e6bbf9d30d94998a7e078ce6d1b155985350169e
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2025-07-21 09:20:54 -0400

    testing commit

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ export default function LandingPage() {
   return (
     <div className="relative flex items-center justify-center min-h-screen bg-custom-dark-gray">
       <CurrentTime />
-      <StreamStatus />
+      <StreamStatus /> 
       <div className="w-full max-w-4xl mx-auto px-4">
         <div className="relative w-full h-0 pb-[56.25%]"> {/* 16:9 aspect ratio */}
-          {/* <iframe 
+          {/* <iframe  
             className="absolute top-0 left-0 w-full h-full rounded-lg"
             src="https://www.youtube.com/embed/live_stream?channel=UCbEmN5Nw2p6yFsaak0sHPpg"
             allowFullScreen
