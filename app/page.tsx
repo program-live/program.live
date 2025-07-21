576435b5e013b4fd0605d8f3c279418c5d247455
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2025-07-21 09:28:32 -0400

    test deploy

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function LandingPage() {
   return (
     <div className="relative flex items-center justify-center min-h-screen bg-custom-dark-gray">
       <CurrentTime />
-      <StreamStatus /> 
+      <StreamStatus />
       <div className="w-full max-w-4xl mx-auto px-4">
         <div className="relative w-full h-0 pb-[56.25%]"> {/* 16:9 aspect ratio */}
           {/* <iframe  
