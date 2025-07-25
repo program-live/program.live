2125e6e24d9b1a3378dd3fcfc222b5c70a4b1bfa
Max Georgopoulos <max123358@gmail.com>
2025-07-25 17:13:02 -0400

    new scrolling text for footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,18 +3,18 @@ import ScrollingText from "./scrolling-text";
 
 // Different language translations of concatenated offline messages
 const translations = [
-  { lang: "EN", text: "SEE YOU SPACE COWBOY • EASY COME, EASY GO • DO YOU HAVE A COMRADE? • LIFE IS BUT A DREAM • ARE YOU LIVING IN THE REAL WORLD? • YOU'RE GONNA CARRY THAT WEIGHT • " },
-  { lang: "ES", text: "NOS VEMOS VAQUERO ESPACIAL • FÁCIL VIENE, FÁCIL SE VA • ¿TIENES UN CAMARADA? • LA VIDA NO ES MÁS QUE UN SUEÑO • ¿ESTÁS VIVIENDO EN EL MUNDO REAL? • VAS A CARGAR ESE PESO • " },
-  { lang: "FR", text: "À BIENTÔT COWBOY DE L'ESPACE • FACILE À VENIR, FACILE À PARTIR • AS-TU UN CAMARADE? • LA VIE N'EST QU'UN RÊVE • VIS-TU DANS LE MONDE RÉEL? • TU VAS PORTER CE POIDS • " },
-  { lang: "DE", text: "BIS DANN WELTRAUM-COWBOY • LEICHT GEKOMMEN, LEICHT GEGANGEN • HAST DU EINEN KAMERADEN? • DAS LEBEN IST NUR EIN TRAUM • LEBST DU IN DER REALEN WELT? • DU WIRST DIESES GEWICHT TRAGEN • " },
-  { lang: "IT", text: "CI VEDIAMO COWBOY SPAZIALE • FACILE VENIRE, FACILE ANDARE • HAI UN COMPAGNO? • LA VITA NON È CHE UN SOGNO • STAI VIVENDO NEL MONDO REALE? • PORTERAI QUEL PESO • " },
-  { lang: "PT", text: "NOS VEMOS COWBOY ESPACIAL • FÁCIL VEM, FÁCIL VAI • VOCÊ TEM UM CAMARADA? • A VIDA NÃO PASSA DE UM SONHO • VOCÊ ESTÁ VIVENDO NO MUNDO REAL? • VOCÊ VAI CARREGAR ESSE PESO • " },
-  { lang: "JA", text: "また会おうスペースカウボーイ • 楽に来て、楽に行く • 仲間はいるか？ • 人生は夢に過ぎない • 君は現実の世界に生きているのか？ • 君はその重荷を背負うことになる • " },
-  { lang: "ZH", text: "再见太空牛仔 • 来得容易，去得容易 • 你有同志吗？ • 人生不过是一场梦 • 你生活在现实世界中吗？ • 你将承受那个重量 • " },
-  { lang: "RU", text: "УВИДИМСЯ КОСМИЧЕСКИЙ КОВБОЙ • ЛЕГКО ПРИШЛО, ЛЕГКО УШЛО • У ТЕБЯ ЕСТЬ ТОВАРИЩ? • ЖИЗНЬ ВСЕГО ЛИШЬ СОН • ТЫ ЖИВЕШЬ В РЕАЛЬНОМ МИРЕ? • ТЫ БУДЕШЬ НЕСТИ ЭТОТ ГРУЗ • " },
-  { lang: "AR", text: "أراك أيها الرعاة الفضائي • سهل المجيء، سهل الذهاب • هل لديك رفيق؟ • الحياة ليست سوى حلم • هل تعيش في العالم الحقيقي؟ • ستحمل ذلك الثقل • " },
-  { lang: "NL", text: "TOT ZIENS RUIMTE COWBOY • MAKKELIJK KOMEN, MAKKELIJK GAAN • HEB JE EEN KAMERAAD? • HET LEVEN IS SLECHTS EEN DROOM • LEEF JE IN DE ECHTE WERELD? • JE GAAT DAT GEWICHT DRAGEN • " },
-  { lang: "KO", text: "또 보자 우주 카우보이 • 쉽게 와서, 쉽게 가고 • 동지가 있나? • 인생은 꿈일 뿐 • 너는 현실 세계에 살고 있나? • 너는 그 무게를 짊어질 것이다 • " }
+  { lang: "EN", text: ["SEE YOU SPACE COWBOY", "EASY COME, EASY GO", "ARE YOU LIVING IN THE REAL WORLD?", "LIFE IS BUT A DREAM"] },
+  { lang: "ES", text: ["NOS VEMOS VAQUERO ESPACIAL", "FÁCIL VIENE, FÁCIL SE VA", "¿ESTÁS VIVIENDO EN EL MUNDO REAL?", "LA VIDA NO ES MÁS QUE UN SUEÑO"] },
+  { lang: "FR", text: ["À BIENTÔT COWBOY DE L'ESPACE", "FACILE À VENIR, FACILE À PARTIR", "VIS-TU DANS LE MONDE RÉEL?", "LA VIE N'EST QU'UN RÊVE"] },
+  { lang: "DE", text: ["BIS DANN WELTRAUM-COWBOY", "LEICHT GEKOMMEN, LEICHT GEGANGEN", "LEBST DU IN DER REALEN WELT?", "DAS LEBEN IST NUR EIN TRAUM"] },
+  { lang: "IT", text: ["CI VEDIAMO COWBOY SPAZIALE", "FACILE VENIRE, FACILE ANDARE", "STAI VIVENDO NEL MONDO REALE?", "LA VITA NON È CHE UN SOGNO"] },
+  { lang: "PT", text: ["NOS VEMOS COWBOY ESPACIAL", "FÁCIL VEM, FÁCIL VAI", "VOCÊ ESTÁ VIVENDO NO MUNDO REAL?", "A VIDA NÃO PASSA DE UM SONHO"] },
+  { lang: "JA", text: ["また会おうスペースカウボーイ", "楽に来て、楽に行く", "君は現実の世界に生きているのか？", "人生は夢に過ぎない"] },
+  { lang: "ZH", text: ["再见太空牛仔", "来得容易，去得容易", "你生活在现实世界中吗？", "人生不过是一场梦"] },
+  { lang: "RU", text: ["УВИДИМСЯ КОСМИЧЕСКИЙ КОВБОЙ", "ЛЕГКО ПРИШЛО, ЛЕГКО УШЛО", "ТЫ ЖИВЕШЬ В РЕАЛЬНОМ МИРЕ?", "ЖИЗНЬ ВСЕГО ЛИШЬ СОН"] },
+  { lang: "AR", text: ["أراك أيها الرعاة الفضائي", "سهل المجيء، سهل الذهاب", "هل تعيش في العالم الحقيقي؟", "الحياة ليست سوى حلم"] },
+  { lang: "NL", text: ["TOT ZIENS RUIMTE COWBOY", "MAKKELIJK KOMEN, MAKKELIJK GAAN", "LEEF JE IN DE ECHTE WERELD?", "HET LEVEN IS SLECHTS EEN DROOM"] },
+  { lang: "KO", text: ["또 보자 우주 카우보이", "쉽게 와서,쉽게 가고", "너는 현실 세계에 살고 있나?", "인생은 꿈일 뿐"] }
 ];
 
 export default function Footer() {
@@ -39,7 +39,7 @@ export default function Footer() {
           >
             {[...Array(50)].map((_, i) => (
               <span key={i}>
-                MARKET.DEV: THE EASIEST WAY TO SELL DEVEL0PER SERVICES <span className="align-[1px]">•</span>{'\u00A0'}
+                MARKET.DEV: THE EASIEST WAY TO SELL DEVEL0PER SERVICES <span className="align-[1px]">◉</span>{'\u00A0'}
               </span>
             ))}
           </ScrollingText>
@@ -62,10 +62,10 @@ export default function Footer() {
                 aria-hidden
               >
                 {[...Array(10)].map((_, i) => (
-                  <span key={i} className="text-10">{translation.text}{'\u00A0'}</span>
+                  <span key={i} className="text-10">{translation.text.join(" ")}{'\u00A0'}</span>
                 ))}
               </ScrollingText>
-              <span className="sr-only">{translation.text}</span>
+              <span className="sr-only">{translation.text.join(' ')}</span>
             </div>
           );
         })}
